bc367a6467341f6f99978440529e69d8fe50ef19
omarr667 <50962383+omarr667@users.noreply.github.com>
2024-11-27 15:30:22 -0600

    Actualizar la versión del paquete a 0.0.1 en setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fcp',
-    version='0.1',
+    version='0.0.1',
     description='Paquete de funciones para el curso de Finanzas Cuantitativas con Python',
     packages=find_packages(),
     install_requires=[
