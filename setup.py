9d5fed05663a6814a995b023032d6a316c1cdebc
omarr667 <50962383+omarr667@users.noreply.github.com>
2024-11-27 16:10:10 -0600

    Actualizar la descripción del paquete y agregar información del autor en setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='fcp',
     version='0.0.1',
-    description='Paquete de funciones para el curso de Finanzas Cuantitativas con Python',
+    description="Un paquete de utilidades para el curso de diplamado de finanzas con Python de la Facultad de Ciencias (UNAM)",  
+    author="Facultad de ciencias",  
+    author_email="omarr667@gmail.com", 
+    url="https://github.com/omarr667/fcp",
     packages=find_packages(include=["fcp", "fcp.*"]),
     include_package_data=True,  
     package_data={
