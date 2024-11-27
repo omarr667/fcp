3e342592a1876d629ca7d8d2400fa7c9075ff9f6
omarr667 <50962383+omarr667@users.noreply.github.com>
2024-11-27 16:07:26 -0600

    Actualizar la versión del paquete a 0.0.1 en setup.py y modificar .gitignore para incluir archivos de byte compilados

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='fcp',
-    version='0.0.2',
+    version='0.0.1',
     description='Paquete de funciones para el curso de Finanzas Cuantitativas con Python',
     packages=find_packages(include=["fcp", "fcp.*"]),
-    include_package_data=True,
+    include_package_data=True,  
+    package_data={
+        "fcp": [
+            "fcp_data/*.db",  
+            "fcp_data/*.json"  
+        ]
+    },
     install_requires=[
+        "pandas", "yfinance", "numpy", "matplotlib", "scipy", "sqlite3"
     ],
 )
