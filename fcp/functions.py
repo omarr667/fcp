02fb56028a64ac8478fb0d2ab1b40c6bd963a53a
omarr667 <50962383+omarr667@users.noreply.github.com>
2025-02-14 19:07:19 -0600

    ``` No se realizaron cambios en el código. ```

--- fcp/functions.py
+++ fcp/functions.py
@@ -8,22 +8,53 @@ import numpy as np
 
 
 def get_universe():
-    return data.get_asset_data('universe')
+    """
+    Devuelve el universo de activos disponibles.
+    Returns:
+        dataFrame de pandas con los activos disponibles.
+    """
+    return data.get_asset_data("universe")
 
 
-def get_assets_data(asset_names, kind='both'):
+def get_assets_data(asset_names, kind="return"):
+    """
+    Devuelve los datos de los activos solicitados.
+    Args:
+        asset_names (str o list): Nombre del activo o lista de nombres de activos.
+        kind (str): Tipo de datos a obtener ('both', 'close', 'return').
+    Returns:
+        dataFrame de pandas con los datos de los activos solicitados.
+    """
+
     if type(asset_names) == str:
         asset_names = [asset_names]
     df = data.get_assets_data_general(asset_names=asset_names, kind=kind)
     return df
 
 
-def get_assets_data_where(query_where, kind='both'):
+def get_assets_data_where(query_where, kind="return"):
+    """
+    Devuelve los datos de los activos que cumplen con la condición especificada.
+    Args:
+        query_where (str): Condición para filtrar los datos.
+        kind (str): Tipo de datos a obtener ('both', 'close', 'return').
+    Returns:
+        dataFrame de pandas con los datos de los activos que cumplen con la condición.
+    """
+
     df = data.get_assets_data_general(query_where=query_where, kind=kind)
     return df
 
 
 def compute_betas(assets, benchmark):
+    """
+    Calcula los betas de los activos con respecto a un benchmark.
+    Args:
+        assets (list): Lista de activos.
+        benchmark (str): Benchmark.
+    Returns:
+        Lista con los betas de los activos.
+    """
     betas = []
     for asset in assets:
         capm = classes.CapitalAssetPricingModel(benchmark, asset)
@@ -33,38 +64,68 @@ def compute_betas(assets, benchmark):
     return betas
 
 
-def cost_function_hedge(x,
-                        position_delta_usd,
-                        position_beta_usd,
-                        hedge_betas,
-                        regularization):
-    f1 = (np.sum(x) + position_delta_usd)**2
-    f2 = (np.dot(hedge_betas,x) + position_beta_usd)**2
-    f3 = regularization * np.sum(np.array(x)**2)
-    f = f1 + f2 + f3
-    return f
-    
+def compute_factors(asset, factors):
+    """
+    Calcula los betas de un activo con respecto a una lista de factores.
+    Args:
+        asset (str): Nombre del activo.
+        factors (list): Lista de factores.
+    Returns:
+        Lista con los betas del activo con respecto a los factores.
 
-def compute_factors(asset, benchmarks):
-    factors = []
-    for benchmark in benchmarks:
-        capm = classes.CapitalAssetPricingModel(benchmark, asset)
+    """
+    betas = []
+    for factor in factors:
+        capm = classes.CapitalAssetPricingModel(factor, asset)
         capm.load_data()
         capm.compute_beta()
-        factors.append(capm.beta)
-    return factors
+        betas.append(capm.beta)
+    return betas
 
 
+def compute_correlations(asset, factors):
+    """
+    Calcula las correlaciones de un activo con respecto a una lista de factores.
+    Args:
+        asset (str): Nombre del activo.
+        factors (list): Lista de factores.
+    Returns:
+        Lista con las correlaciones del activo con respecto a los factores.
+    """
+    correl = []
+    for factor in factors:
+        capm = classes.CapitalAssetPricingModel(factor, asset)
+        capm.load_data()
+        capm.compute_beta()
+        correl.append(capm.correlation)
+    return correl
 
 
+def cost_function_hedge(
+    x, position_delta_usd, position_beta_usd, hedge_betas, regularization
+):
+    f1 = (np.sum(x) + position_delta_usd) ** 2
+    f2 = (np.dot(hedge_betas, x) + position_beta_usd) ** 2
+    f3 = regularization * np.sum(np.array(x) ** 2)
+    f = f1 + f2 + f3
+    return f
 
 
-
+# def compute_factors(asset, benchmarks):
+#     factors = []
+#     for benchmark in benchmarks:
+#         capm = classes.CapitalAssetPricingModel(benchmark, asset)
+#         capm.load_data()
+#         capm.compute_beta()
+#         factors.append(capm.beta)
+#     return factors
 
 
 # Forma 1: Correlación y varianza
-def compute_corr_cov_matrix(data_df, classification='Return'):
+def compute_corr_cov_matrix(data_df, classification="Return"):
     """
+    No se recomienda usar esta función, ya que es más eficiente calcular las matrices por separado.
+
     Calcula las matrices de correlación y covarianza para un conjunto de datos filtrado.
 
     Parámetros:
@@ -77,18 +138,26 @@ def compute_corr_cov_matrix(data_df, classification='Return'):
     Lanza:
     - ValueError: Si no se encuentran columnas que coincidan con la clasificación.
     """
-    filtered_data_df = data_df.filter(like=classification) if classification else data_df
+    filtered_data_df = (
+        data_df.filter(like=classification) if classification else data_df
+    )
     if filtered_data_df.empty:
-        raise ValueError(f"No se encontraron columnas que contengan la subcadena '{classification}'.")
+        raise ValueError(
+            f"No se encontraron columnas que contengan la subcadena '{classification}'."
+        )
 
     corr_matrix = filtered_data_df.corr()
     cov_matrix = filtered_data_df.cov()
 
-    return {'correlation': corr_matrix, 'covariance': cov_matrix}
+    print(
+        "Advertencia: No se recomienda usar esta función para calcular ambas matrices simultáneamente. Usa mejor df.cov()"
+    )
+
+    return {"correlation": corr_matrix, "covariance": cov_matrix}
 
 
 # Forma 2: Elemento por elemento
-def compute_correlation_matrix(data_df, classification='Return'):
+def compute_correlation_matrix(data_df, classification="Return"):
     """
     Calcula la matriz de correlación para un conjunto de datos filtrado.
 
@@ -102,15 +171,24 @@ def compute_correlation_matrix(data_df, classification='Return'):
     Lanza:
     - ValueError: Si no se encuentran columnas que coincidan con la clasificación.
     """
-    filtered_data_df = data_df.filter(like=classification) if classification else data_df
+    filtered_data_df = (
+        data_df.filter(like=classification) if classification else data_df
+    )
     if filtered_data_df.empty:
-        raise ValueError(f"No se encontraron columnas que contengan la subcadena '{classification}'.")
+        raise ValueError(
+            f"No se encontraron columnas que contengan la subcadena '{classification}'."
+        )
 
     corr_matrix = filtered_data_df.corr()
+
+    print(
+        "Advertencia: No se recomienda usar esta función para calcular la matriz de correlación. Usa mejor df.corr()."
+    )
+
     return corr_matrix
 
 
-def compute_covariance_matrix(data_df, classification='Return'):
+def compute_covariance_matrix(data_df, classification="Return"):
     """
     Calcula la matriz de covarianza para un conjunto de datos filtrado.
 
@@ -124,17 +202,20 @@ def compute_covariance_matrix(data_df, classification='Return'):
     Lanza:
     - ValueError: Si no se encuentran columnas que coincidan con la clasificación.
     """
-    filtered_data_df = data_df.filter(like=classification) if classification else data_df
+    filtered_data_df = (
+        data_df.filter(like=classification) if classification else data_df
+    )
     if filtered_data_df.empty:
-        raise ValueError(f"No se encontraron columnas que contengan la subcadena '{classification}'.")
+        raise ValueError(
+            f"No se encontraron columnas que contengan la subcadena '{classification}'."
+        )
 
     cov_matrix = filtered_data_df.cov()
     return cov_matrix
 
 
-
 # Forma 3: Conjunta con mapeo de valores, separada por comas
-def compute_corr_cov_matrix_map(data_df, classification='Return'):
+def compute_corr_cov_matrix_map(data_df, classification="Return"):
     """
     Calcula las matrices de correlación y covarianza para un conjunto de datos filtrado y las retorna como una tupla.
 
@@ -148,9 +229,13 @@ def compute_corr_cov_matrix_map(data_df, classification='Return'):
     Lanza:
     - ValueError: Si no se encuentran columnas que coincidan con la clasificación.
     """
-    filtered_data_df = data_df.filter(like=classification) if classification else data_df
+    filtered_data_df = (
+        data_df.filter(like=classification) if classification else data_df
+    )
     if filtered_data_df.empty:
-        raise ValueError(f"No se encontraron columnas que contengan la subcadena '{classification}'.")
+        raise ValueError(
+            f"No se encontraron columnas que contengan la subcadena '{classification}'."
+        )
 
     corr_matrix = filtered_data_df.corr()
     cov_matrix = filtered_data_df.cov()
@@ -158,7 +243,7 @@ def compute_corr_cov_matrix_map(data_df, classification='Return'):
     return corr_matrix, cov_matrix
 
 
-def plot_assets(data_df, classification='Return', style='matplotlib', **kwargs):
+def plot_assets(data_df, classification="Return", style="matplotlib", **kwargs):
     """
     Grafica los datos de los activos utilizando el estilo seleccionado.
 
@@ -174,59 +259,70 @@ def plot_assets(data_df, classification='Return', style='matplotlib', **kwargs):
     Lanza:
     - ValueError: Si el estilo proporcionado no es soportado.
     """
-    filtered_data_df = data_df.filter(like=classification) if classification else data_df
+    filtered_data_df = (
+        data_df.filter(like=classification) if classification else data_df
+    )
     if filtered_data_df.empty:
-        raise ValueError(f"No se encontraron columnas que contengan la subcadena '{classification}' para graficar.")
+        raise ValueError(
+            f"No se encontraron columnas que contengan la subcadena '{classification}' para graficar."
+        )
 
-    if style == 'matplotlib':
+    if style == "matplotlib":
         import matplotlib.pyplot as plt
+
         plt.figure(figsize=(10, 6))
-        
+
         for asset_name in filtered_data_df.columns:
-            plt.plot(filtered_data_df.index, filtered_data_df[asset_name], label=asset_name)
-        
+            plt.plot(
+                filtered_data_df.index, filtered_data_df[asset_name], label=asset_name
+            )
+
         # Obtiene los parámetros adicionales o usa valores por defecto
-        title = kwargs.get('title', f'{classification} de los Activos')
-        x_label = kwargs.get('xaxis_title', 'Fecha')
-        y_label = kwargs.get('yaxis_title', classification)
-        
+        title = kwargs.get("title", f"{classification} de los Activos")
+        x_label = kwargs.get("xaxis_title", "Fecha")
+        y_label = kwargs.get("yaxis_title", classification)
+
         plt.title(title)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
-        
-        plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+
+        plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
         plt.grid(True)
         plt.tight_layout()
         plt.show()
-    
-    elif style == 'plotly':
+
+    elif style == "plotly":
         import plotly.graph_objects as go
         import plotly.io as pio
 
-        pio.renderers.default = 'browser'
+        pio.renderers.default = "browser"
 
         fig = go.Figure()
 
         for column in filtered_data_df.columns:
-            fig.add_trace(go.Scatter(
-                x=filtered_data_df.index, 
-                y=filtered_data_df[column], 
-                mode='lines', 
-                name=column
-            ))
+            fig.add_trace(
+                go.Scatter(
+                    x=filtered_data_df.index,
+                    y=filtered_data_df[column],
+                    mode="lines",
+                    name=column,
+                )
+            )
 
         # Actualiza el layout con los parámetros adicionales
         fig.update_layout(
-            title=kwargs.get('title', f'{classification} de los Activos'),
-            xaxis_title=kwargs.get('xaxis_title', 'Fecha'),
-            yaxis_title=kwargs.get('yaxis_title', classification),
-            **kwargs.get('layout', {})
+            title=kwargs.get("title", f"{classification} de los Activos"),
+            xaxis_title=kwargs.get("xaxis_title", "Fecha"),
+            yaxis_title=kwargs.get("yaxis_title", classification),
+            **kwargs.get("layout", {}),
         )
 
         fig.show()
-    
+
     else:
-        raise ValueError("El estilo proporcionado no es válido. Usa 'matplotlib' o 'plotly'.")
+        raise ValueError(
+            "El estilo proporcionado no es válido. Usa 'matplotlib' o 'plotly'."
+        )
 
 
 def plot_comparing_two_timeseries(assets, assets_df):
@@ -243,8 +339,10 @@ def plot_comparing_two_timeseries(assets, assets_df):
     try:
         # Verifica que la lista de activos tenga al menos dos elementos
         if len(assets) < 2:
-            raise ValueError("La lista de activos debe contener al menos dos elementos.")
-        
+            raise ValueError(
+                "La lista de activos debe contener al menos dos elementos."
+            )
+
         # Define los nombres de los activos
         asset_1 = assets[0]
         asset_2 = assets[1]
@@ -252,38 +350,51 @@ def plot_comparing_two_timeseries(assets, assets_df):
         # Copia y prepara el data_dfFrame para graficar
         df2 = assets_df.copy()
         df2 = df2.reset_index()  # Asegura que 'Date' esté disponible como columna
-        
+
         # Configura la figura
         plt.figure(figsize=(12, 6))
-        plt.title('Comparación de Series de Tiempo de Precios de Cierre', fontsize=14)
-        plt.xlabel('Fecha', fontsize=12)
-        plt.ylabel('Precio', fontsize=12)
-        
+        plt.title("Comparación de Series de Tiempo de Precios de Cierre", fontsize=14)
+        plt.xlabel("Fecha", fontsize=12)
+        plt.ylabel("Precio", fontsize=12)
+
         # Obtiene el eje actual para asignar múltiples líneas
         ax = plt.gca()
 
         # Grafica la primera serie de tiempo
         df2.plot(
-            kind='line', x='Date', y=f'{asset_1}_Close', ax=ax, grid=True, 
-            color='blue', label=f'{asset_1} (Cierre)'
+            kind="line",
+            x="Date",
+            y=f"{asset_1}_Close",
+            ax=ax,
+            grid=True,
+            color="blue",
+            label=f"{asset_1} (Cierre)",
         )
 
         # Grafica la segunda serie de tiempo en un eje secundario
         df2.plot(
-            kind='line', x='Date', y=f'{asset_2}_Close', ax=ax, grid=True, 
-            color='red', secondary_y=True, label=f'{asset_2} (Cierre)'
+            kind="line",
+            x="Date",
+            y=f"{asset_2}_Close",
+            ax=ax,
+            grid=True,
+            color="red",
+            secondary_y=True,
+            label=f"{asset_2} (Cierre)",
         )
 
         # Configura las leyendas
-        ax.legend(loc='upper left', fontsize=10)
-        ax.right_ax.legend(loc='upper right', fontsize=10)  # Leyenda del eje secundario
+        ax.legend(loc="upper left", fontsize=10)
+        ax.right_ax.legend(loc="upper right", fontsize=10)  # Leyenda del eje secundario
 
         # Muestra la gráfica
         plt.tight_layout()
         plt.show()
 
     except KeyError as e:
-        print(f"Error: La columna especificada no se encuentra en el data_dfFrame. Detalle: {e}")
+        print(
+            f"Error: La columna especificada no se encuentra en el data_dfFrame. Detalle: {e}"
+        )
     except ValueError as e:
         print(f"Error en los datos: {e}")
     except Exception as e:
